7be118dae0cd10fedbf25af2a76562e342ca1aad
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-09-02 15:25:11 -0400

    adds NewFeed forms to FeedsContainer display

--- src/containers/FeedsContainer.js
+++ src/containers/FeedsContainer.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Feed from '../components/feeds/Feed'
+import NewFeed from '../components/forms/NewFeed'
 
 class FeedsContainer extends Component {
 
+  whatToRender = () => {
+    if(this.props.feeds.length === 4){
+      return this.props.feeds.map(feed => <Feed feed={feed} key={feed.handle} />)
+    } else if (this.props.feeds.length > 0){
+      const feeds = this.props.feeds.map(feed => <Feed feed={feed} key={feed.handle} />)
+      const forms = []
+      for(let i=0; i<(4-this.props.feeds.length); i++){
+        forms.push(<NewFeed key={i}/>)
+      }
+      return feeds.concat(forms)
+    } else {
+      const forms = []
+      for(let i=0; i<4; i++){
+        forms.push(<NewFeed key={i} />)
+      }
+      return forms
+    }
+  }
   render() {
 
     return (
       <div className='feeds-container'>
-        {this.props.feeds.map(feed => <Feed feed={feed} key={feed.handle} />)}
+        {this.whatToRender()}
       </div>
     )
   }
