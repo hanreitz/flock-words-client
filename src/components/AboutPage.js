ee12ed6d4e663a570fbf2fbba006ff681268999e
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-09-05 17:21:34 -0400

    adds GitHub links to About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,7 +6,7 @@ export default function AboutPage() {
       <h3 className='page-title'>About</h3>
       <p>This application was made using the Twitter API</p>
 
-      <p>The GitHub repositories for this project are available here (backend) and here (frontend).</p>
+      <p>The GitHub repositories for this project are available <a href='https://github.com/hanreitz/flock-words-api'>here</a> (backend) and <a href='https://github.com/hanreitz/flock-words-client'>here</a> (frontend).</p>
 
       <p>To learn more about the development of this project, please check out my blog post.</p>
     </div>
